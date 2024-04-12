c66e2a664c9f00c3b1f37fc1634f594a5311b279
gugububu <james0787@naver.com>
2024-04-12 18:34:00 +0900

    2

--- finetuning-app/src/Chat.js
+++ finetuning-app/src/Chat.js
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from "react";
+// use state는 함수형 컴포넌트에 상태를 추가하는 데 사용됨.
+//usEffect는 컴포넌트에서 사이트 이펙트를 수행하는데 사용.
 import Typewriter from 'react-ts-typewriter';
+//To use Typing effect
 import "./App.css";
+
+  //"React Hooks allows 
+  //you to use state and side effects in functional components."
 
 const ChatScreen = () => {
   const [messages, setMessages] = useState([]);
+  //usestate훅을 사용ㅎ아ㅕ massage와 currentTypingId라는 두가지 상태변수 선언
   const [currentTypingId, setCurrentTypingId] = useState(null);
+  //currentTypingId )현재 ai가 작성하는 메시지 추척
+
+ //일반적으로 usestate는 두개의 인자를 받는데, 첫번째는  초기 상태를 나타내는 인자, 
+ //두개의 요소를 가진 배열을 반환함.
 
   const handleSendMessage = (message) => {
     setMessages((prevMessages) => [
@@ -24,6 +35,8 @@ const ChatScreen = () => {
       prevMessages.map((msg) =>
         msg.id === id ? { ...msg, isTyping: false } : msg
       )
+      // usestate 훅이 반환하는 setMessages 함수를 호출하는 부분. 
+      // setMessages 함수는 message 상태를 업데이트 하는 데 사용됨
     );
     setCurrentTypingId(null);
   };
